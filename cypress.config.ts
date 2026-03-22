5b6f3c2362c95cda21d36dc04aff41164012cb2a
Arti Singh <artisingh@Artis-MacBook-Pro.local>
2026-03-22 09:40:31 -0400

    added a cypress config with urls obfuscated

---
+++ cypress.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "cypress";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export default defineConfig({
+  allowCypressEnv: true,
+  screenshotOnRunFailure: true,
+  scrollBehavior: 'top',
+  taskTimeout: 60000,
+  trashAssetsBeforeRuns: true,
+  video: true,
+  videoCompression: true,
+  viewportHeight: 720,
+  viewportWidth: 1280,
+  waitForAnimations: true,
+  watchForFileChanges: true,
+  defaultCommandTimeout:10000,
+
+  e2e: {
+    slowTestThreshold: 10000,
+    baseUrl: process.env.CYPRESS_BASE_URL,
+    supportFile: 'cypress/support/e2e.ts',
+    supportFolder: 'cypress/support',
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+    testIsolation: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
